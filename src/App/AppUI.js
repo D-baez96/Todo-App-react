48fe12fff623f60a81321ad2530885c6e0629a14
DanDev <daniel-trabajo@hotmail.com>
2023-06-26 23:03:33 -0500

    Todo App orginizada

---
+++ src/App/AppUI.js
@@ -0,0 +1,39 @@
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton/index.js';
+
+function AppUI ({
+    completedTodos,
+    totalTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo
+}){
+    return (
+        <>
+        <TodoCounter completed = {completedTodos} total = {totalTodos} />
+        <TodoSearch 
+            searchValue = {searchValue}
+            setSearchValue = {setSearchValue}
+        />
+
+            <TodoList>
+            {searchedTodos.map (todo => (
+            <TodoItem 
+                key={todo.text} 
+                text ={todo.text}
+                completed = {todo.completed}
+                onComplete = {() => completeTodo (todo.text)}
+                onDelete = {() => deleteTodo (todo.text)}
+            />
+            ))}
+        </TodoList>
+        <CreateTodoButton/>
+        </>
+    );
+}
+export {AppUI};
---
+++ src/App/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { AppUI } from './AppUI';
+import { useLocalStorage } from './UseLocalStorage';
+
+ //const defaultTodos= [
+  //{ text : "Aprender React", completed: true},
+  //{ text : "Tomar Tinto", completed: false},
+  //{ text : "Lavar La loza", completed: false},
+  //{ text : "Tender la cama", completed: false},
+ //];
+
+//localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+//localStorage.removeItem('TODOS_V1');
+
+function App() {
+
+  const [todos,saveTodos] = useLocalStorage('TODOS_V1',[]);
+  const [searchValue, setSearchValue]= React.useState('');
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos =  todos.length;
+  const searchedTodos = todos.filter(
+    (todo) =>{
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    } 
+  );
+
+  const completeTodo = (text) =>{
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) =>{
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex,1);
+    saveTodos(newTodos);
+  };
+  return (
+    <AppUI
+    completedTodos = {completedTodos}
+    totalTodos = {totalTodos}
+    searchValue = {searchValue}
+    setSearchValue = {setSearchValue}
+    searchedTodos = {searchedTodos}
+    completeTodo = {completeTodo}
+    deleteTodo = {deleteTodo}
+    />
+);
+}
+
+export default App;
